db3fcc2ad4607a13c19fb9be47e05544f82562b6
asap-addy <106650076+asap-addy@users.noreply.github.com>
2023-04-23 17:59:08 -0700

    Update app.py

--- Weather App/app.py
+++ Weather App/app.py
@@ -1,63 +1,15 @@
 import tkinter as tk
-from tkinter import messagebox
 import requests
-import urllib.parse
 
-def get_weather(city):
-    """enter api key in quotes"""
-    #api_key = 
-    base_url = "http://api.openweathermap.org/data/2.5/weather"
-    params = {
-        "q": urllib.parse.quote(city),
-        "appid": api_key,
-        "units": "metric",
-        "exclude": "minutely",  
-    }
-    response = requests.get(base_url, params=params)
-    data = response.json()
-    return data
-
-def show_weather():
-    city = city_entry.get()  # Get city name from input field
-    weather_data = get_weather(city)  # Get weather data from API
-
-    if "cod" in weather_data and weather_data["cod"] == "404":
-        # Display error message if city not found
-        messagebox.showerror("Error", "City not found. Please try again.")
-    else:
-        # Extract weather information
-        temperature = weather_data.get("main", {}).get("temp", "N/A")
-        humidity = weather_data.get("main", {}).get("humidity", "N/A")
-        pressure = weather_data.get("main", {}).get("pressure", "N/A")
-        lightning = weather_data.get("lightning", "N/A")
-        rain = weather_data.get("rain", {}).get("1h", "N/A")
-        wind_speed = weather_data.get("wind", {}).get("speed", "N/A")
-        snow = weather_data.get("snow", {}).get("1h", "N/A")
-        air_quality = weather_data.get("aqi", {}).get("value", "N/A")
-
-        # Update labels with weather information
-        temperature_label.config(text=f"Temperature: {temperature} °C")
-        humidity_label.config(text=f"Humidity: {humidity}%")
-        pressure_label.config(text=f"Pressure: {pressure} hPa")
-        lightning_label.config(text=f"Lightning: {lightning}")
-        rain_label.config(text=f"Rain (1h): {rain} mm")
-        wind_speed_label.config(text=f"Wind Speed: {wind_speed} m/s")
-        snow_label.config(text=f"Snow (1h): {snow} mm")
-        air_quality_label.config(text=f"Air Quality Index: {air_quality}")
-
-# Create GUI window
+# Create main window
 root = tk.Tk()
 root.title("Weather App")
 
-# Create labels and entry field for city input
+# Create labels and entry for city input
 city_label = tk.Label(root, text="Enter City:")
-city_label.pack(pady=10)
+city_label.pack()
 city_entry = tk.Entry(root)
-city_entry.pack(pady=10)
-
-# Create button to get weather information
-get_weather_button = tk.Button(root, text="Get Weather", command=show_weather)
-get_weather_button.pack(pady=10)
+city_entry.pack()
 
 # Create labels to display weather information
 temperature_label = tk.Label(root, text="")
@@ -77,6 +29,7 @@ snow_label.pack()
 air_quality_label = tk.Label(root, text="")
 air_quality_label.pack()
 
+# Function to update weather information labels
 def update_weather_labels(temperature, humidity, pressure, lightning, rain, wind_speed, snow, air_quality):
     temperature_label.config(text=f"Temperature: {temperature} °C")
     humidity_label.config(text=f"Humidity: {humidity}%")
@@ -87,5 +40,65 @@ def update_weather_labels(temperature, humidity, pressure, lightning, rain, wind
     snow_label.config(text=f"Snow (1h): {snow} mm")
     air_quality_label.config(text=f"Air Quality Index: {air_quality}")
 
-# Run the GUI loop
+# Function to get weather data from OpenWeatherMap API
+def get_weather():
+    city = city_entry.get()
+    city = city.replace(" ", "%20")  # Replace spaces with '%20'
+    #enter api key below in quotes
+    #api_key = 
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise exception for non-200 status codes
+        data = response.json()
+        temperature = data['main']['temp']
+        humidity = data['main']['humidity']
+        pressure = data['main']['pressure']
+        if 'lightning' in data['weather'][0]['description'].lower():
+            lightning = 'Yes'
+        else:
+            lightning = 'No'
+        if 'rain' in data.keys():
+            rain = data['rain']['1h']
+        else:
+            rain = 0
+        if 'wind' in data.keys():
+            wind_speed = data['wind']['speed']
+        else:
+            wind_speed = 0
+        if 'snow' in data.keys():
+            snow = data['snow']['1h']
+        else:
+            snow = 0
+        if 'aqi' in data.keys() and 'us-epa' in data['aqi'].keys():
+            air_quality = data['aqi']['us-epa']['aqi']
+        else:
+            air_quality = 'N/A'
+        update_weather_labels(temperature, humidity, pressure, lightning, rain, wind_speed, snow, air_quality)
+    except requests.exceptions.HTTPError as e:
+        # Handle HTTP errors
+        temperature_label.config(text="Error: Failed to fetch weather data")
+        humidity_label.config(text="")
+        pressure_label.config(text="")
+        lightning_label.config(text="")
+        rain_label.config(text="")
+        wind_speed_label.config(text="")
+        snow_label.config(text="")
+        air_quality_label.config(text="")
+    except Exception as e:
+        # Handle other exceptions
+        temperature_label.config(text="Error: An unexpected error occurred")
+        humidity_label.config(text="")
+        pressure_label.config(text="")
+        lightning_label.config(text="")
+        rain_label.config(text="")
+        wind_speed_label.config(text="")
+        snow_label.config(text="")
+        air_quality_label.config(text="")
+
+# Create button to get weather
+get_weather_button = tk.Button(root, text="Get Weather", command=get_weather)
+get_weather_button.pack()
+
+# Run tkinter event loop
 root.mainloop()
